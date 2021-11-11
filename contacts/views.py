c1d0328774c5504d7fe090041ed8a40ecdce9a58
nahyer <36651110+nahyer@users.noreply.github.com>
2021-11-11 19:42:20 +0700

    Delete unnecessary import

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,5 @@
-from rest_framework import status, viewsets, generics
-from rest_framework.decorators import api_view, action
+from rest_framework import viewsets, generics
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Contact, ContactList
 from .serializers import ContactSerializer, ContactListSerializer
