14cc4581efb6d7778f08641df407363dca1a6ea9
nahyer <36651110+nahyer@users.noreply.github.com>
2021-11-14 06:59:35 +0700

    Delete unnecessary attr on models

--- contacts/models.py
+++ contacts/models.py
@@ -3,17 +3,22 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    phone_num = models.CharField(max_length=15, blank=False, null=False)
-    name = models.CharField(max_length=60, blank=True, default=phone_num)
+    phone_num = models.CharField(max_length=15)
+    name = models.CharField(max_length=60, blank=True)
     email_address = models.EmailField(max_length=320, blank=True, default='')
+
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = self.phone_num
+        super(Contact, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
 class ContactList(models.Model):
-    name = models.CharField(max_length=60, blank=False, null=False)
-    contacts = models.ManyToManyField(Contact, related_name='contact_list', blank=True)
+    name = models.CharField(max_length=60)
+    contacts = models.ManyToManyField(Contact, related_name='contact_list')
 
     def __str__(self):
         return self.name
