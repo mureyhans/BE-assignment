e64e21d79243328fa451dfd96d5068dcd6f0af45
nahyer <36651110+nahyer@users.noreply.github.com>
2021-11-14 07:01:07 +0700

    Use necessary Views only based on requirements

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, re_path, include
 from . import views
-from contacts.views import ContactViewSet, ContactListViewSet
+from contacts.views import ContactListViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('api/contacts', ContactViewSet, basename='contact')
 router.register('api/contact-lists', ContactListViewSet, basename='contact-list')
-# router.urls
 
 urlpatterns = [
+    path('api/contacts/', views.ContactListView.as_view(), name='contact-list'),
+    path('api/contacts/<int:pk>/', views.ContactDetailView.as_view(), name='contact-detail'),
+    path('api/contacts/<int:pk>/contact-lists', views.ContactDetailView.contact_list, name='contact-contact-lists'),
     path('', include(router.urls)),
     re_path(r'^api/contact-lists/name/(?P<name>.+)/$', views.ContactListDetail.as_view(), name='contact-list-search'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,49 +1,77 @@
 from rest_framework import viewsets, generics
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from .models import Contact, ContactList
 from .serializers import ContactSerializer, ContactListSerializer
 
 
 # Create your views here.
-
-class ContactViewSet(viewsets.ModelViewSet):
+class ContactListView(generics.ListCreateAPIView):
     """
-    list:
-    Return a list of all the existing Contacts.
+    get:
+    Return a list of all the existing contacts
+    Implements `GET /api/contacts/` endpoint
 
-    retrieve:
-    Return the given Contact.
-
-    create:
-    Create a new Contact instance.
+    post:
+    Create a new contact
+    Implements `POST /api/contacts/` endpoint
     """
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+
+class ContactDetailView(generics.RetrieveDestroyAPIView):
+    """
+    get:
+    Return the given Contact
+    Implements `GET /api/contacts/{id}/` endpoint
+
+    delete:
+    Delete the given Contact
+    Implements `DELETE /api/contacts/{id}/` endpoint
+    """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    @api_view(['GET'])
+    def contact_list(self, pk):
+        """
+        get:
+        Return a list of Contact List for given Contact.
+        Implements `GET /api/contacts/{id}/contact-lists`
+        """
+        contact_lists = ContactList.objects.filter(contacts__id=pk)
+        serializer = ContactListSerializer(contact_lists, many=True)
+        return Response(serializer.data)
 
 
 class ContactListViewSet(viewsets.ModelViewSet):
     """
     list:
     Return a list of all the existing Contact Lists.
+    Implements `GET /api/contact-lists/`
 
     retrieve:
     Return the given Contact List.
+    Implements `GET /api/contact-lists/{id}`
 
     create:
     Create a new Contact List instance.
+    Implements `POST /api/contact-lists/`
 
     partial_update:
     Update partially the given Contact List. Can be use to Add or Remove Contacts from a Contact List
+    Implements `POST /api/contact-lists/{id}`
     """
     queryset = ContactList.objects.all()
     serializer_class = ContactListSerializer
 
     @action(detail=True)
-    def get_contacts(self, request, pk):
+    def contacts(self, request, pk):
         """
         get:
         Return a list of Contacts within given Contact List.
+        Implements `GET /api/contact-lists/{id}/contacts`
         """
         contacts = Contact.objects.filter(contact_list__id=pk)
         serializer = ContactSerializer(contacts, many=True)
@@ -54,6 +82,7 @@ class ContactListDetail(generics.ListAPIView):
     """
     get:
     Return list of Contact List based on name
+    Implements `GET /api/contact-lists/name/{name}`
     """
     serializer_class = ContactListSerializer
 
